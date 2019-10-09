fc1429e4e5fdad392cf2e9d1f400a3794f7e6780
Jonathan Deng <jonathan.j.deng@gmail.com>
2019-10-09 13:23:26 -0400

    Refactor adjoint test script

--- tests/test_adjoint.py
+++ tests/test_adjoint.py
@@ -15,156 +15,156 @@ sys.path.append('../')
 from femvf.forward import forward
 from femvf.adjoint import adjoint
 from femvf import forms
-from femvf import constants
+from femvf.constants import DEFAULT_FLUID_PROPERTIES, DEFAULT_SOLID_PROPERTIES, PASCAL_TO_CGS
 from femvf import functionals
-from femvf import statefileutils as sfu
+from femvf import statefile as sfu
 
-if __name__ == '__main__':
-    dfn.set_log_level(30)
+sys.path.append(os.path.expanduser('~/GraduateSchool/Projects/FEMVFOptimization/'))
+import functionals as extra_functionals
 
-    ### Finite Differences
-    print("Computing Gradient via Finite Differences")
+dfn.set_log_level(30)
+np.random.seed(123)
 
-    ## Mesh Setup
-    mesh_dir = '../meshes'
-
-    mesh_base_filename = 'geometry2'
-    mesh_path = os.path.join(mesh_dir, mesh_base_filename + '.xml')
-
-    model = forms.ForwardModel(mesh_path, {'pressure': 1, 'fixed': 3}, {})
-
-    emod = model.emod.vector()[:].copy()
-    emod[:] = 10e3 * constants.PASCAL_TO_CGS
-    step_size = 0.01*constants.PASCAL_TO_CGS
-    num_steps = 7
-
-    # Set fluid and solid properties
-    fluid_props = constants.DEFAULT_FLUID_PROPERTIES.copy()
-
-    # Time varying fluid properties
-    # fluid_props = constants.DEFAULT_FLUID_PROPERTIES
-    # fluid_props['p_sub'] = [1500 * constants.PASCAL_TO_CGS, 1500 * constants.PASCAL_TO_CGS, 1, 1]
-    # fluid_props['p_sub_time'] = [0, 3e-3, 3e-3, 0.02]
-
-    solid_props = constants.DEFAULT_SOLID_PROPERTIES.copy()
-    solid_props['rayleigh_m'] = 1e-3
-    solid_props['rayleigh_k'] = 1e-3
-
-    dt = 1e-4
-    times_meas = [0, 0.01]
-
-    ## Running finite differences
-    save_path = 'out/FiniteDifferenceStates.h5'
-    with h5py.File(save_path, mode='w') as f:
-        f.create_dataset('elastic_modulus', data=emod[0])
-        f.create_dataset('step_size', data=step_size)
-        f.create_dataset('num_steps', data=num_steps)
-
-    for ii in range(num_steps):
-        solid_props['elastic_modulus'] = emod + ii*step_size
-
-        runtime_start = perf_counter()
-        forward(model, 0, times_meas, dt, solid_props, fluid_props, h5file=save_path,
-                h5group=f'{ii}/')
-        runtime_end = perf_counter()
-
-        print(f"Runtime {runtime_end-runtime_start:.2f} seconds")
+save_path = 'out/FiniteDifferenceStates.h5'
 
 
-    ### Adjoint
-    print("Computing Gradient via Adjoint State")
+## Set the mesh to be used and initialize the forward model
+mesh_dir = '../meshes'
 
-    ## Different functional setups
-    # Functional for vocal eff
-    # n_start = 50
-    # fkwargs = {'n_start': n_start}
-    # dg_du = functionals.dtotalvocaleff_du
-    # functional = functionals.totalvocaleff
+mesh_base_filename = 'geometry2'
+mesh_path = os.path.join(mesh_dir, mesh_base_filename + '.xml')
 
-    # Functional for MFDR
-    # fkwargs = {}
-    # dg_du = functionals.dmfdr_du
-    # functional = functionals.mfdr
+model = forms.ForwardModel(mesh_path, {'pressure': 1, 'fixed': 3}, {})
 
-    # Functional for weighted sum of squared glottal widths
-    # fkwargs = {}
-    # dg_du = functionals.dwss_gwidth_du
-    # functional = functionals.wss_gwidth
 
-    # Functional for total flow
-    n_start = 0
-    fkwargs = {'n_start': n_start}
-    dg_du = functionals.dtotalflow_du
-    functional = functionals.totalflow
+## Set the solution parameters
+dt = 1e-4
+times_meas = [0, 0.01]
 
+
+## Set the fluid/solid parameters
+emod = model.emod.vector()[:].copy()
+emod[:] = 10e3 * PASCAL_TO_CGS
+step_size = 1e1 * PASCAL_TO_CGS
+num_steps = 7
+
+emod_dir = np.random.rand(emod.size)
+
+fluid_props = DEFAULT_FLUID_PROPERTIES.copy()
+
+# Time varying fluid properties
+# fluid_props = constants.DEFAULT_FLUID_PROPERTIES
+# fluid_props['p_sub'] = [1500 * constants.PASCAL_TO_CGS, 1500 * constants.PASCAL_TO_CGS, 1, 1]
+# fluid_props['p_sub_time'] = [0, 3e-3, 3e-3, 0.02]
+
+solid_props = DEFAULT_SOLID_PROPERTIES.copy()
+solid_props['rayleigh_m'] = 1e-3
+solid_props['rayleigh_k'] = 1e-3
+
+
+## Set a functional
+dg_dparam = None
+fkwargs = {}
+# Functional for vocal eff
+# n_start = 50
+# fkwargs = {'n_start': n_start}
+functional = functionals.totalvocaleff
+dg_du = functionals.dtotalvocaleff_du
+
+# Functional for MFDR
+# fkwargs = {}
+# functional = functionals.mfdr
+# dg_du = functionals.dmfdr_du
+
+# Functional for weighted sum of squared glottal widths
+# fkwargs = {}
+# functional = functionals.wss_gwidth
+# dg_du = functionals.dwss_gwidth_du
+
+# Functional for total flow
+# n_start = 0
+# fkwargs = {'n_start': n_start}
+# functional = functionals.totalflow
+# dg_du = functionals.dtotalflow_du
+
+# Functional for acoustic efficiency
+# functional = extra_functionals.acoustic_efficiency
+# dg_du = extra_functionals.dacoustic_efficiency_du
+
+
+## Finite Differences
+print("Computing Gradient via Finite Differences")
+
+if os.path.exists(save_path):
+    os.remove(save_path)
+
+for n in range(num_steps):
     runtime_start = perf_counter()
-    info = None
-    with sfu.StateFile(save_path, group='0') as f:
-        _, info = functional(model, f, **fkwargs)
-    gradient = adjoint(model, save_path, h5group='0', dg_du=dg_du, dg_du_kwargs={**fkwargs, **info})
+    solid_props['elastic_modulus'] = emod + n*step_size*emod_dir
+    forward(model, 0, times_meas, dt, solid_props, fluid_props, h5file=save_path, h5group=f'{n}')
     runtime_end = perf_counter()
 
     print(f"Runtime {runtime_end-runtime_start:.2f} seconds")
 
-    with h5py.File('out/Adjoint.h5', mode='w') as f:
-        f.create_dataset('gradient', data=gradient)
+# Calculate functional values at each finite difference step
+functional_fd = list()
+with sfu.StateFile('out/FiniteDifferenceStates.h5', group='0', mode='r') as f:
+    for n in range(num_steps):
+        f.group = f'{n}'
+        functional_fd.append(functional(model, f, **fkwargs)[0])
 
-    ### Comparing adjoint and finite differences
-    # Load data and caculate gradient from FD steps
-    emod = None
-    cost_fd = list()
-    with sfu.StateFile('out/FiniteDifferenceStates.h5', group=f'{ii}', mode='r') as f:
-        # import ipdb; ipdb.set_trace()
-        step_size = f.file['step_size'][()]
-        num_steps = f.file['num_steps'][()]
-        emod = f.file['elastic_modulus'] + np.arange(num_steps)*step_size
-        for ii in range(num_steps):
-            f.group = f'{ii}'
-            # import ipdb; ipdb.set_trace()
-            cost_fd.append(functional(model, f, **fkwargs)[0])
+functional_fd = np.array(functional_fd)
 
-    # Load the gradient from the adjoint method
-    grad_ad = None
-    with h5py.File('out/Adjoint.h5', mode='r') as f:
-        grad_ad = f['gradient'][...]
 
-    ### Figure generation
-    fig, axs = plt.subplots(1, 2, figsize=(7, 3))
+## Adjoint
+print("Computing Gradient via Adjoint State")
 
-    ## Plotting
-    cost_fd = np.array(cost_fd)
-    axs[0].plot(emod, cost_fd, color='C0', marker='o', label="Functional from F")
+runtime_start = perf_counter()
+info = None
+with sfu.StateFile(save_path, group='0') as f:
+    _, info = functional(model, f, **fkwargs)
+gradient_ad = adjoint(model, save_path, h5group='0', dg_du=dg_du, dg_du_kwargs={**fkwargs, **info},
+                      dg_dparam=dg_dparam, dg_dparam_kwargs={**fkwargs, **info})
+runtime_end = perf_counter()
 
-    # Project the gradient in the direction of uniform increase in elastic modulus
-    demod = emod-emod[0]
-    grad_ad_projected = grad_ad.sum()
-    cost_ad = cost_fd[0] + grad_ad_projected*demod
-    axs[0].plot(emod, cost_ad, color='C1', marker='o', label="Linear prediction from gradient")
+print(f"Runtime {runtime_end-runtime_start:.2f} seconds")
 
-    grad_fd_projected = (cost_fd[1:]-cost_fd[0])/(demod[1:])
-    error = np.abs((grad_ad_projected-grad_fd_projected)/grad_ad_projected)*100
-    axs[1].plot(np.arange(error.size)+1, error)
 
-    ## Formatting
-    axs[0].set_xlabel("Elastic modulus [Pa]")
-    axs[0].set_ylabel("Objective function")
-    axs[0].legend()
+## Comparing adjoint and finite difference projected gradients
+fig, axs = plt.subplots(1, 2, figsize=(7, 3))
 
-    axs[1].set_ylabel(r"% Error")
+step_points = step_size * np.arange(num_steps)
+axs[0].plot(step_points, functional_fd, color='C0', marker='o',
+            label="Directly computed functionals")
 
-    for ax in axs:
-        ax.grid()
+# Project the gradient in the direction of elastic modulus increase
+grad_ad_projected = np.sum(gradient_ad * emod_dir)
+functional_ad = functional_fd[0] + grad_ad_projected*step_points
+axs[0].plot(step_points, functional_ad, color='C1', marker='o',
+            label="Linear prediction from gradient")
 
-    axs[0].set_xlabel(r"$E$")
-    axs[1].set_xlabel(r"$N_{\Delta h}$")
+grad_fd_projected = (functional_fd[2:]-functional_fd[:-2])/(step_points[2:] - step_points[:-2])
+error = np.abs((grad_ad_projected-grad_fd_projected)/grad_ad_projected)*100
+axs[1].plot(1 + np.arange(error.size), error)
 
-    axs[0].set_xlim(emod[[0, -1]])
-    axs[1].set_xlim([0, error.size])
-    axs[1].set_ylim([0, error.max()])
+# Formatting
+axs[0].set_xlim(step_points[[0, -1]])
+axs[0].set_xlabel("Elastic modulus [Pa]")
+axs[0].set_ylabel("Objective function")
+axs[0].legend()
 
-    plt.tight_layout()
-    plt.show()
+for ax in axs:
+    ax.grid()
 
-    print(f"Linear gradient prediction {grad_ad_projected:.16e}")
-    print(f"Actual FD values {grad_fd_projected[0]:.16e}")
-    print(f"% Error {error}")
+axs[1].set_xlim([0, error.size])
+axs[1].set_ylim([0, error.max()])
+
+axs[1].set_xlabel(r"$N_{\Delta h}$")
+axs[1].set_ylabel(r"% Error")
+
+plt.tight_layout()
+plt.show()
+
+print(f"Linear gradient prediction {grad_ad_projected:.16e}")
+print(f"Actual FD values {grad_fd_projected[0]:.16e}")
+print(f"% Error {error}")
