e36c4b65cb497db33149f30923171c3cc0b285ce
Jon Deng <jonathan.j.deng@gmail.com>
2021-06-12 15:00:14 -0400

    Fix test_acoustic to compare against analytical solution
    test_acoustic.py now tests the impulse response from WRA against a known
    analytical solution for a 1D tube. The results compare very well so you
    can be confident the implementation of WRA works.

--- tests/test_acoustic.py
+++ tests/test_acoustic.py
@@ -17,8 +17,7 @@ class TestWRA(unittest.TestCase):
         # self.DT = 2*self.LENGTH/self.NTUBE/self.CSOUND
         # print(self.DT)
 
-        model = WRAnalog(self.NTUBE)
-        self.model = model
+        self.model = WRAnalog(self.NTUBE)
 
     def test_uniform(self):
         ## Set properties of the uniform tube
@@ -27,30 +26,29 @@ class TestWRA(unittest.TestCase):
         props['proploss'][:] = 1.0
         props['rhoac'][:] = 1.2e-3
         props['length'][:] = self.LENGTH
-
+        # self.model.dt = self.LENGTH/self.model.NTUBE
         # Uniform tract
         props['area'][:] = 4.0
-
         # Idealized /a/ 
-        props['area'][:22] = 0.5
-        props['area'][22:] = 3
-
+        # props['area'][:22] = 0.5
+        # props['area'][22:] = 3
         self.model.set_properties(props)
 
+        # Set the impulse flow input
         state0 = self.model.get_state_vec()
         state0.set(0.0)
-
-        control = self.model.get_control_vec()
-        control.set(0.0)
-        control['qin'][:] = 1.0
-
         qin = 1.0
         state0['pref'][:2] = self.model.inputq(qin, state0['pinc'][:2])
+        control = self.model.get_control_vec()
+        control.set(0.0)
+        # control['qin'][:] = 0.0
 
+        ## Set the input function and integrate over time
         tstart = perf_counter()
-        times = self.model.dt*np.arange(2**12)
+        times = self.model.dt*np.arange(2**14)
         pinc = np.zeros((times.size, state0['pinc'].size))
         pref = np.zeros((times.size, state0['pref'].size))
+        # pref[0, :] = state0['pref']
         for n in range(1, times.size):
             self.model.set_ini_state(state0)
             self.model.set_control(control)
@@ -62,20 +60,46 @@ class TestWRA(unittest.TestCase):
             state0 = state1
         print(f"Duration {perf_counter()-tstart:.4f} s")
 
-        # Compute the impulse response of the radiated pressure
+        ## Numerical impulse response from WRA radiated pressure
         prad = pref[:, -1]
         N = prad.size
         ft_prad = np.fft.fft(prad)
         ft_freq = np.fft.fftfreq(N, d=self.model.dt)
 
         fig, ax = plt.subplots(1, 1)
-        ax.plot(ft_freq[0:N//2], 20*np.log10(np.abs(ft_prad[0:N//2])))
+        ax.plot(ft_freq[0:N//2], 20*np.log10(np.abs(ft_prad[0:N//2])), label='WRA')
 
-        ax.set_ylabel("Frequency response [db]")
+        ## Theoretical impulse response from uniform tube anlalytical solution
+        omega = ft_freq*2*np.pi
+        RHO = props['rhoac']
+        LENGTH, CSOUND = props['length'], props['soundspeed']
+        AMOUTH = props['area'][-1]
+        PISTON_RAD = np.sqrt(AMOUTH/np.pi)
+        ZMOUTH = RHO*CSOUND/AMOUTH
+
+        R = 128/(9*np.pi**2) * ZMOUTH
+        L = 8*PISTON_RAD/(3*np.pi*CSOUND) * ZMOUTH
+        z_rad = (1j*omega*R*L)/(R+1j*omega*L)
+
+        z0 = ZMOUTH
+        gamma_ref = (z_rad-z0)/(z_rad+z0)
+        z_tube = z0*(1+gamma_ref)*np.exp(-1j*omega*LENGTH/CSOUND)/(1-gamma_ref*np.exp(-2j*omega*LENGTH/CSOUND)) 
+        # breakpoint()
+
+        qin_vec = np.zeros(times.size)
+        qin_vec[0] = qin
+        ft_qin = np.fft.fft(qin_vec)
+        ax.plot(ft_freq[0:N//2], 20*np.log10(np.abs((ft_qin*z_tube)[0:N//2])), label='Ideal')
+
+        ax.set_xticks(np.arange(0, ft_freq.max(), 500))
+        ax.set_ylabel("Frequency response [db]") 
         ax.set_xlabel("Frequency [Hz]")
         ax.set_xlim(0, 5000)
         ax.grid()
-        fig.savefig('out/acoustics-uniform-impulseresponse.png')
+        ax.legend()
+
+        fig.tight_layout()
+        fig.savefig(f'out/acoustics-uniform-impulseresponse-2e{np.log2(times.size):.0f}.png')
 
 
 if __name__ == '__main__':
