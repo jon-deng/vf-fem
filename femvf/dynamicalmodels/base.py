08e4bb077e181dccbe441ccef6940e45a8c9829b
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-02-03 15:07:14 -0500

    Add more functions to dynamical system residuals

--- femvf/dynamicalmodels/base.py
+++ femvf/dynamicalmodels/base.py
@@ -6,5 +6,14 @@ class DynamicalSystemResidual:
 
     # def __init__()
 
-    # def set_state()
-    # def set_parameters()
+    def set_state(state):
+        self.state[:] = state
+
+    def set_statet(statet):
+        self.statet[:] = statet
+
+    def set_icontrol(icontrol):
+        self.icontrol[:] = icontrol
+        
+    def set_properties(props):
+        self.properties[:] = props
--- femvf/dynamicalmodels/solid.py
+++ femvf/dynamicalmodels/solid.py
@@ -19,14 +19,34 @@ class SolidDynamicalSystemResidual(DynamicalSystemResidual):
                                             cell_func, cell_label_to_id, fsi_facet_labels, fixed_facet_labels)
         gen_residual_bilinear_forms(self._forms)
 
-       
-        self.state1 = BlockVec((self.u1.vector(), self.v1.vector(), self.a1.vector()), ('u', 'v', 'a'))
-        self.icontrol1 = BlockVec((self.forms['coeff.fsi.p1'].vector(),), ('p',))
+        self.u = self.forms['coeff.state.u1']
+        self.v = self.forms['coeff.state.v1']
+        self.state = BlockVec((self.u.vector(), self.v.vector()), ('u', 'v'))
+
+        self.ut = dfn.Function(self.forms['coeff.state.u1'].function_space())
+        self.vt = self.forms['coeff.state.a1']
+        self.statet = BlockVec((self.ut.vector(), self.vt.vector()), ('ut', 'vt'))
+
+        self.icontrol = BlockVec((self.forms['coeff.fsi.p1'].vector(),), ('p',))
         self.properties = self.get_properties_vec(set_default=True)
         self.set_properties(self.properties)
 
-    # def form_defin
+    @property
+    def forms(self):
+        return self._forms
 
     def get_properties_vec(self, set_default=True):
         defaults = self.PROPERTY_DEFAULTS if set_default else None
         return properties_bvec_from_forms(self.forms, defaults)
+
+    def set_properties(self, props):
+        for key in props.keys:
+            # TODO: Check types to make sure the input property is compatible with the solid type
+            coefficient = self.forms['coeff.prop.'+key]
+
+            # If the property is a field variable, values have to be assigned to every spot in
+            # the vector
+            if isinstance(coefficient, dfn.function.constant.Constant):
+                coefficient.assign(dfn.Constant(np.squeeze(props[key])))
+            else:
+                coefficient.vector()[:] = props[key]
