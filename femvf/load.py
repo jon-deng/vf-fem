24425cd416328d0fbefabc932dc5623041340d39
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-02-12 14:22:16 -0500

    Add load function for dynamical FSI models in load.py

--- femvf/load.py
+++ femvf/load.py
@@ -8,7 +8,8 @@ from . import meshutils
 from .models import solid as smd, fluid as fmd
 from .models.coupled import (FSAIModel, ExplicitFSIModel, ImplicitFSIModel)
 
-# from .dynamicalmodels import solid as smd, fluid as fmd
+from .dynamicalmodels import solid as dynsolidmodel, fluid as dynfluidmodel, coupled as dynfsimodel
+
 # from .dynamicalmodels.coupled import (FSAIModel, ExplicitFSIModel, ImplicitFSIModel)
 
 def load_solid_model(solid_mesh, SolidType, 
@@ -43,6 +44,8 @@ def load_fsi_model(solid_mesh, fluid_mesh, SolidType=smd.KelvinVoigt, FluidType=
 
     Parameters
     ----------
+    solid_mesh : dolfin.Mesh
+    fluid_mesh : None or (future proofing for other fluid types)
     """
     solid, fluid, fsi_verts = process_fsi(solid_mesh, fluid_mesh, 
         SolidType=SolidType, FluidType=FluidType, fsi_facet_labels=fsi_facet_labels, fixed_facet_labels=fixed_facet_labels)
@@ -53,6 +56,29 @@ def load_fsi_model(solid_mesh, fluid_mesh, SolidType=smd.KelvinVoigt, FluidType=
         model = ImplicitFSIModel(solid, fluid, fsi_verts)
 
     return model
+
+def load_dynamical_fsi_model(
+    solid_mesh, fluid_mesh, 
+    SolidType=dynsolidmodel.KelvinVoigt, FluidType=dynfluidmodel.Bernoulli1DDynamicalSystem, 
+    fsi_facet_labels=('pressure',), fixed_facet_labels=('fixed',)):
+    """
+    Factory function that loads a model based on input solid/fluid meshes.
+
+    The function will delegate to specific loading routines depending on how the mesh
+    is input etc.
+
+    Parameters
+    ----------
+    solid_mesh : dolfin.Mesh
+    fluid_mesh : None or (future proofing for other fluid types)
+    """
+    solid, fluid, fsi_verts = process_fsi(solid_mesh, fluid_mesh, 
+        SolidType=SolidType, FluidType=FluidType, fsi_facet_labels=fsi_facet_labels, fixed_facet_labels=fixed_facet_labels)
+    
+    dofs_fsi_solid = dfn.vertex_to_dof_map(solid.forms['fspace.scalar'])[fsi_verts]
+    dofs_fsi_fluid = np.arange(dofs_fsi_solid.size)
+
+    return dynfsimodel.FSIDynamicalSystem(solid, fluid, dofs_fsi_solid, dofs_fsi_fluid)
 
 def load_fsai_model(solid_mesh, fluid_mesh, acoustic, SolidType=smd.KelvinVoigt, FluidType=fmd.Bernoulli, 
                     fsi_facet_labels=('pressure',), fixed_facet_labels=('fixed',), coupling='explicit'):
@@ -93,12 +119,19 @@ def process_fsi(solid_mesh, fluid_mesh, SolidType=smd.KelvinVoigt, FluidType=fmd
     fsi_verts = fsi_verts[idx_sort]
 
     # Load the fluid
-    fluid_model = None
+    # There are separate cases for loading the transient fluid model and the dynamical system fluid
+    # model because I changed the way the mesh is input between them
     if fluid_mesh is None and issubclass(FluidType, fmd.QuasiSteady1DFluid):
-        xfluid, yfluid = meshutils.streamwise1dmesh_from_edges(mesh, facet_func, [facet_labels[label] for label in fsi_facet_labels])
-        fluid_model = FluidType(xfluid, yfluid)
-    elif fluid_mesh is None and not issubclass(FluidType, fmd.QuasiSteady1DFluid):
-        raise ValueError(f"`fluid_mesh` cannot be `None` if the fluid is not 1D")
+        x, y = meshutils.streamwise1dmesh_from_edges(
+            mesh, facet_func, [facet_labels[label] for label in fsi_facet_labels])
+        fluid = FluidType(x, y)
+    elif fluid_mesh is None and issubclass(FluidType, dynfluidmodel.BaseFluid1DDynamicalSystem):
+        x, y = meshutils.streamwise1dmesh_from_edges(
+            mesh, facet_func, [facet_labels[label] for label in fsi_facet_labels])
+        dx = x[1:] - x[:-1]
+        dy = y[1:] - y[:-1]
+        s = np.array(np.concatenate([0], np.cumsum(np.sqrt(dx**2 + dy**2))))
+        fluid = FluidType(s)
     else:
         raise ValueError(f"This function does not yet support input fluid meshes.")
-    return solid, fluid_model, fsi_verts
+    return solid, fluid, fsi_verts
