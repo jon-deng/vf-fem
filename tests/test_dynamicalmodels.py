fac8cb44567defe970f2566d41e2cb2487dfc5bc
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-02-28 19:11:59 -0500

    Fix bad ycontact value in test_dynamicalmodels

--- tests/test_dynamicalmodels.py
+++ tests/test_dynamicalmodels.py
@@ -4,6 +4,9 @@ This modules contains tests for dynamical models
 
 from os import path
 # from functools import partial
+import warnings
+import numpy as np
+import dolfin as dfn
 
 from blocklinalg import linalg as bla
 from blocklinalg import genericops as gops
@@ -11,6 +14,8 @@ from blocklinalg import genericops as gops
 from femvf.dynamicalmodels import solid as slmodel, fluid as flmodel
 from femvf import load
 
+# warnings.filterwarnings('error', 'RuntimeWarning')
+np.seterr(invalid='raise')
 
 mesh_name = 'M5-3layers'
 mesh_path = path.join('../meshes', mesh_name+'.xml')
@@ -26,14 +31,16 @@ model_coupled = load.load_dynamical_fsi_model(
 model_solid = model_coupled.solid
 model_fluid = model_coupled.fluid
 
-props_fluid = model_fluid.properties.copy()
-props_fluid['psub'] = 800*10
-props_fluid['psup'] = 0.0
-model_fluid.set_properties(props_fluid)
+props = model_coupled.properties.copy()
+props['psub'] = 800*10
+props['psup'] = 0.0
+props['ycontact'] = np.max(model_solid.XREF.vector()[1::2]) + 1 # 1 cm above the maximum y extent
+model_coupled.ymid = props['ycontact'][0]
+model_coupled.set_properties(props)
 
 model = model_solid
-model = model_fluid
-model = model_coupled
+# model = model_fluid
+# model = model_coupled
 
 def gen_res(x, set_x, assem_resx):
     set_x(x)
@@ -49,14 +56,25 @@ def test_assem_dres_dstate():
 
     x0 = model.state.copy()
     dx = x0.copy()
-    if 'u' in dx:
-        gops.set_vec(dx['u'], 1e-3)
-        gops.set_vec(dx['v'], 0)
-    elif 'q' in dx:
+    if 'u' in dx and 'v' in dx:
+        dxu = model_solid.state['u'].copy()
+        dxu[:] = 1e-3
+        dxu[:] = 1e-3*np.arange(dxu[:].size)
+        model.forms['bc.dirichlet'].apply(dxu)
+        gops.set_vec(dx['u'], dxu)
+
+        dxv = model_solid.state['v'].copy()
+        dxv[:] = 0.0
+        model.forms['bc.dirichlet'].apply(dxv)
+        gops.set_vec(dx['v'], dxv)
+    if 'q' in dx:
         dx.set(1e-3)
     x1 = x0 + dx
+    model.set_state(x1)
+    breakpoint()
 
     dres_exact = res(x1) - res(x0)
+    # breakpoint()
     A = jac(x0)
     dres_linear = bla.mult_mat_vec(jac(x0), dx)
     print(dres_linear.norm(), dres_exact.norm())
@@ -68,8 +86,8 @@ def test_assem_dres_dstatet():
     x0 = model.statet.copy()
     dx = x0.copy()
     if 'u' in dx:
-        dx['u'][:] = 1e-3
-        dx['v'][:] = 0
+        gops.set_vec(dx['u'][:], 1e-3)
+        gops.set_vec(dx['v'][:], 0)
     elif 'q' in dx:
         dx.set(1e-3)
     x1 = x0 + dx
