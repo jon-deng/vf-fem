a379923f281e7d03d7392d9bfee108ba102fbf6b
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-03-28 20:09:01 -0400

    Add Taylor convergence test to test_dynamicalmodels

--- tests/test_dynamicalmodels.py
+++ tests/test_dynamicalmodels.py
@@ -1,5 +1,5 @@
 """
-This modules contains tests for dynamical models 
+This module contains tests for dynamical models 
 """
 
 from os import path
@@ -15,7 +15,7 @@ from femvf.dynamicalmodels import solid as slmodel, fluid as flmodel
 from femvf import load
 
 # warnings.filterwarnings('error', 'RuntimeWarning')
-np.seterr(invalid='raise')
+# np.seterr(invalid='raise')
 
 ### Configuration ###
 ## Loading the model to test
@@ -38,42 +38,48 @@ model_coupled = load.load_dynamical_fsi_model(
 
 model_solid = model_coupled.solid
 model_fluid = model_coupled.fluid
-# model = model_solid
+model = model_solid
 # model = model_fluid
-model = model_coupled
+# model = model_coupled
 
 ## Set the model properties/parameters
 props = model_coupled.properties.copy()
 props['emod'].array[:] = 5e3*10
 props['rho'].array[:] = 1.0
-# props['psub'][:] = 800*10
-# props['psup'][:] = 0.0
-props['ycontact'][:] = np.max(model_solid.XREF.vector()[1::2]) + 1 # 1 cm above the maximum y extent
-model_coupled.ymid = props['ycontact'][0]
+
+ymax = np.max(model_solid.XREF.vector()[1::2])
+ygap = 0.01 # gap between VF and symmetry plane
+ymid = ymax + ygap
+ycontact = ymid - 0.1*ygap
+props['ycontact'][:] = ycontact # 1 cm above the maximum y extent
+model_coupled.ymid = ymid
 model_coupled.set_properties(props)
 
+props['zeta_sep'][0] = 1e-3
+props['zeta_min'][0] = 1e-3
+props['rho_air'][0] = 1.0e-3
+
 ## Set the linearization point and linearization directions to test along
-# state
 state0 = model.state.copy()
 dstate = state0.copy()
 if 'u' in dstate and 'v' in dstate:
     dxu = model_solid.state['u'].copy()
     dxu[:] = 1e-3*np.arange(dxu[:].size)
-    dxu[:] = 1e-7
+    dxu[:] = 1e-8
     # dxu[:] = 0
     model_solid.forms['bc.dirichlet'].apply(dxu)
     gops.set_vec(dstate['u'], dxu)
 
     dxv = model_solid.state['v'].copy()
-    dxv[:] = 0.0
+    dxv[:] = 1e-8
     model_solid.forms['bc.dirichlet'].apply(dxv)
     gops.set_vec(dstate['v'], dxv)
-# if 'q' in dstate:
-#     gops.set_vec(dstate['q'], 1e-3)
-#     # gops.set_vec(dstate['q'], 0.0)
-# if 'p' in dstate:
-#     gops.set_vec(dstate['p'], 1e-3)
-#     # gops.set_vec(dstate['p'], 0.0)
+if 'q' in dstate:
+    gops.set_vec(dstate['q'], 1e-3)
+    # gops.set_vec(dstate['q'], 0.0)
+if 'p' in dstate:
+    gops.set_vec(dstate['p'], 1e-3)
+    # gops.set_vec(dstate['p'], 0.0)
 
 statet0 = state0.copy()
 dstatet = dstate.copy()
@@ -85,7 +91,7 @@ if hasattr(model, 'control'):
         control0['psub'][:] = 800*10
     if 'psup' in control0:
         control0['psup'][:] = 0
-    dcontrol.set(1e-5)
+    dcontrol.set(1e0)
 model.set_control(control0)
 
 props0 = model.properties.copy()
@@ -100,19 +106,36 @@ def gen_jac(x, set_x, assem_jacx):
     set_x(x)
     return assem_jacx()
 
+def _test_taylor(x0, dx, res, jac):
+    """
+    Test that the Taylor convergence order is 2
+    """
+    res0 = res(x0)
+    alphas = 2**np.arange(4)[::-1] # start with the largest step and move to original
+    dres_exacts = [res(x0+alpha*dx)-res0 for alpha in alphas] 
+    dres_linear = bla.mult_mat_vec(jac(x0), dx)
+    errs = [
+        (dres_exact-alpha*dres_linear).norm() 
+        for dres_exact, alpha in zip(dres_exacts, alphas)]
+    with np.errstate(invalid='ignore'):
+        conv_rates = [
+            np.log(err_0/err_1)/np.log(alpha_0/alpha_1)
+            for err_0, err_1, alpha_0, alpha_1 
+            in zip(errs[:-1], errs[1:], alphas[:-1], alphas[1:])]
+
+    print("")
+    print(f"||dres_linear||, ||dres_exact|| = {dres_linear.norm()}, {dres_exacts[-1].norm()}")
+    print("Errors: ", errs)
+    print("Convergence rates: ", conv_rates)
+
 def test_assem_dres_dstate():
     res = lambda state: gen_res(state, model.set_state, model.assem_res)
     jac = lambda state: gen_jac(state, model.set_state, model.assem_dres_dstate)
 
     x0 = state0
     dx = dstate
-    x1 = x0 + dx
-    # model.set_state(x1)
 
-    dres_exact = res(x1) - res(x0)
-    dres_linear = bla.mult_mat_vec(jac(x0), dx)
-    print([subvec.norm() for subvec in dres_linear])
-    print(dres_linear.norm(), dres_exact.norm())
+    _test_taylor(x0, dx, res, jac)
 
 def test_assem_dres_dstatet():
     res = lambda state: gen_res(state, model.set_statet, model.assem_res)
@@ -120,11 +143,8 @@ def test_assem_dres_dstatet():
 
     x0 = statet0
     dx = 1e-5*dstatet
-    x1 = x0 + dx
 
-    dres_exact = res(x1) - res(x0)
-    dres_linear = bla.mult_mat_vec(jac(x0), dx)
-    print(dres_linear.norm(), dres_exact.norm())
+    _test_taylor(x0, dx, res, jac)
 
 def test_assem_dres_dcontrol():
     # model_fluid.control['psub'][:] = 1
@@ -134,12 +154,8 @@ def test_assem_dres_dcontrol():
 
     x0 = control0
     dx = dcontrol
-    x1 = x0 + dx
 
-    dres_exact = res(x1) - res(x0)
-    dres_linear = bla.mult_mat_vec(jac(x0), dx)
-    # breakpoint()
-    print(dres_linear.norm(), dres_exact.norm())
+    _test_taylor(x0, dx, res, jac)
 
 def test_assem_dres_dprops():
     res = lambda state: gen_res(state, model.set_properties, model.assem_res)
@@ -147,16 +163,16 @@ def test_assem_dres_dprops():
 
     x0 = props0
     dx = dprops
-    x1 = x0 + dx
 
-    dres_exact = res(x1) - res(x0)
-    dres_linear = bla.mult_mat_vec(jac(x0), dx)
-    print(dres_exact)
-    print(dres_linear.norm(), dres_exact.norm())
+    _test_taylor(x0, dx, res, jac)
 
 if __name__ == '__main__':
     # breakpoint()
+    print("-- Testing dres/dstate --")
     test_assem_dres_dstate()
+
+    print("\n-- Testing dres/dstatet --")
     test_assem_dres_dstatet()
-    if hasattr(model, 'control'):
-        test_assem_dres_dcontrol()
+
+    print("\n-- Testing dres/dcontrol --")
+    test_assem_dres_dcontrol()
