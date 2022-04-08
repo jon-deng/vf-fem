cdaa033f4cb6a312d08e65dbbc0374a2eee9c5fc
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-04-08 12:20:41 -0400

    WIP: Refactor models to subpackage

--- femvf/models/dynamical/coupled.py
+++ femvf/models/dynamical/coupled.py
@@ -17,59 +17,6 @@ from .solid import BaseSolidDynamicalSystem
 
 # pylint: disable=missing-function-docstring
 
-class FSIMap:
-    """
-    Represents a mapping between two domains (fluid and solid)
-
-    This mapping involves a 1-to-1 correspondence between DOFs of vectors on the two domains
-    """
-    def __init__(self, ndof_fluid, ndof_solid, fluid_dofs, solid_dofs, comm=None):
-        """
-        Parameters
-        ----------
-        ndof_fluid, ndof_solid : int
-            number of DOFS on the fluid and solid domains
-        fluid_dofs, solid_dofs : array
-            arrays of corresponding dofs on the fluid and solid side domains
-        comm : None or PETSc.Comm
-            MPI communicator. Not really used here since I never run stuff in parallel.
-        """
-        self.N_FLUID = ndof_fluid
-        self.N_SOLID = ndof_solid
-
-        self.dofs_fluid = fluid_dofs
-        self.dofs_solid = solid_dofs
-
-        self.fluid_to_solid_idx = {idxf: idxs for idxf, idxs in zip(fluid_dofs, solid_dofs)}
-        self.solid_to_fluid_idx = {idxs: idxf for idxf, idxs in zip(fluid_dofs, solid_dofs)}
-
-        self.dsolid_dfluid = self.assem_dsolid_dfluid(comm)
-        self.dfluid_dsolid = self.assem_dfluid_dsolid(comm)
-
-    def map_fluid_to_solid(self, fluid_vec, solid_vec):
-        solid_vec[self.dofs_solid] = fluid_vec[self.dofs_fluid]
-
-    def map_solid_to_fluid(self, solid_vec, fluid_vec):
-        fluid_vec[self.dofs_fluid] = solid_vec[self.dofs_solid]
-
-    def assem_dsolid_dfluid(self, comm=None):
-        # pylint: disable=no-member
-        A = PETSc.Mat().createAIJ([self.N_SOLID, self.N_FLUID], comm=comm)
-        A.setUp()
-        for jj, ii in self.fluid_to_solid_idx.items():
-            A.setValue(ii, jj, 1)
-        A.assemble()
-        return A
-
-    def assem_dfluid_dsolid(self, comm=None):
-        # pylint: disable=no-member
-        A = PETSc.Mat().createAIJ([self.N_FLUID, self.N_SOLID], comm=comm)
-        A.setUp()
-        for jj, ii in self.solid_to_fluid_idx.items():
-            A.setValue(ii, jj, 1)
-        A.assemble()
-        return A
-
 class FSIDynamicalSystem(DynamicalSystem):
     """
     Class representing a fluid-solid coupled dynamical system
