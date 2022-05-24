18f958ffbf3f54d2a743bc7ba29ca3554c9a94a1
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-05-24 11:05:44 -0400

    Refactor test_dynamicalsolid.py style and docstrings

--- tests/test_dynamicalsolid.py
+++ tests/test_dynamicalsolid.py
@@ -4,22 +4,18 @@ Test that the Hopf solid models will run
 
 import os
 import dolfin as dfn
-from pprint import pprint
 
 from femvf.meshutils import load_fenics_xmlmesh
-# from femvf.models import solid as smd
 from femvf.models.dynamical import solid as dynsol
 
 dfn.set_log_level(30)
 
-mesh_dir = '../meshes'
-mesh_name = 'M5-3layers'
-mesh_path = os.path.join(mesh_dir, mesh_name + '.xml')
-
-mesh, facet_func, cell_func, (vertex_labels, facet_label_to_id, cell_label_to_id) = \
-    load_fenics_xmlmesh(mesh_path)
-
-def test_dynamical_kelvin_voigt():
+def test_dynamical_kelvin_voigt(mesh_path):
+    """
+    Test that the Kelvin Voigt model can be loaded
+    """
+    mesh, facet_func, cell_func, (vertex_labels, facet_label_to_id, cell_label_to_id) = \
+        load_fenics_xmlmesh(mesh_path)
     # breakpoint()
     fsi_facet_labels = ['pressure']
     fixed_facet_labels = ['fixed']
@@ -32,4 +28,7 @@ def test_dynamical_kelvin_voigt():
     model.assem_dres_dcontrol()
 
 if __name__ == '__main__':
-    print(test_dynamical_kelvin_voigt())
+    mesh_dir = '../meshes'
+    mesh_name = 'M5-3layers'
+    mesh_path = os.path.join(mesh_dir, mesh_name + '.xml')
+    test_dynamical_kelvin_voigt(mesh_path)
