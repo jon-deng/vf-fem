2cae35e592270e251e1464f9a4aa8421df6f69ad
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-05-29 16:03:18 -0400

    Add dynamical system with fixed separation point

--- femvf/models/dynamical/fluid.py
+++ femvf/models/dynamical/fluid.py
@@ -14,21 +14,21 @@ import numpy as np
 from blockarray import blockvec as bla
 
 from .base import DynamicalSystem
-from ..equations.fluid.bernoulli_sep_at_min import (
-    bernoulli_qp,
-    dbernoulli_qp,
-    dbernoulli_qp_darea,
-    dbernoulli_qp_dpsub,
-    dbernoulli_qp_dpsup,
-    ddbernoulli_qp_darea,
-    ddbernoulli_qp_dpsub,
-    ddbernoulli_qp_dpsup
-)
+from ..equations.fluid import bernoulli_sep_at_min as bernmin
+from ..equations.fluid import bernoulli_sep_at_fixed as bernfix
+
+# pylint: disable=missing-docstring
 
 class BaseFluid1DDynamicalSystem(DynamicalSystem):
 
     def __init__(self, s):
         self.s = s
+
+
+class BaseBernoulli1DDynamicalSystem(BaseFluid1DDynamicalSystem):
+
+    def __init__(self, s):
+        super().__init__(s)
 
         N = self.s.size
         self.q = np.zeros(1)
@@ -51,7 +51,7 @@ class BaseFluid1DDynamicalSystem(DynamicalSystem):
         self.props = bla.BlockVector(
             [np.ones(1) for i in range(len(properties_vec))], labels=[properties_vec])
 
-class Bernoulli1DDynamicalSystem(BaseFluid1DDynamicalSystem):
+class Bernoulli1DDynamicalSystem(BaseBernoulli1DDynamicalSystem):
     def assem_res(self):
         # Depack variables from BlockVector for input to Bernoulli functions
         area = self.control['area']
@@ -62,7 +62,7 @@ class Bernoulli1DDynamicalSystem(BaseFluid1DDynamicalSystem):
         zeta_sep = self.props['zeta_sep'][0]
 
         primals = (area, self.s, psub, psup, rho, zeta_min, zeta_sep)
-        qp_explicit = bernoulli_qp(*primals)
+        qp_explicit = bernmin.bernoulli_qp(*primals)
         resq = self.q - qp_explicit[0]
         resp = self.p - qp_explicit[1]
         return bla.BlockVector([resq, resp], labels=self.state.labels)
@@ -99,9 +99,9 @@ class Bernoulli1DDynamicalSystem(BaseFluid1DDynamicalSystem):
         zeta_sep = self.props['zeta_sep'][0]
         primals = (area, self.s, psub, psup, rho, zeta_min, zeta_sep)
 
-        dq_darea, dp_darea = dbernoulli_qp_darea(*primals)
-        dq_dpsub, dp_dpsub = dbernoulli_qp_dpsub(*primals)
-        dq_dpsup, dp_dpsup = dbernoulli_qp_dpsup(*primals)
+        dq_darea, dp_darea = bernmin.dbernoulli_qp_darea(*primals)
+        dq_dpsub, dp_dpsub = bernmin.dbernoulli_qp_dpsub(*primals)
+        dq_dpsup, dp_dpsup = bernmin.dbernoulli_qp_dpsup(*primals)
 
         mats = [
             [-dq_darea, -dq_dpsub, -dq_dpsup],
@@ -124,9 +124,7 @@ class Bernoulli1DDynamicalSystem(BaseFluid1DDynamicalSystem):
             for state_subvec in self.state]
         return bla.BlockMatrix(mats, labels=(self.state.labels[0], self.props.labels[0]))
 
-
-
-class LinearizedBernoulli1DDynamicalSystem(BaseFluid1DDynamicalSystem):
+class LinearizedBernoulli1DDynamicalSystem(BaseBernoulli1DDynamicalSystem):
     def assem_res(self):
         # Depack variables from BlockVector for input to Bernoulli functions
         area = self.control['area']
@@ -143,7 +141,7 @@ class LinearizedBernoulli1DDynamicalSystem(BaseFluid1DDynamicalSystem):
         tangents = (darea, np.zeros(self.s.shape), dpsub, dpsup, 0.0, 0.0, 0.0)
 
         # The linearized residual changes due to linearizations
-        resq_bernoulli, resp_bernoulli = dbernoulli_qp(*primals, tangents)
+        resq_bernoulli, resp_bernoulli = bernmin.dbernoulli_qp(*primals, tangents)
 
         resq = self.dstate['q'] - resq_bernoulli
         resp = self.dstate['p'] - resp_bernoulli
@@ -187,9 +185,184 @@ class LinearizedBernoulli1DDynamicalSystem(BaseFluid1DDynamicalSystem):
         dpsup = self.dcontrol['psup']
         tangents = (darea, np.zeros(self.s.shape), dpsub, dpsup, 0.0, 0.0, 0.0)
 
-        dq_darea, dp_darea = ddbernoulli_qp_darea(*primals, tangents)
-        dq_dpsub, dp_dpsub = ddbernoulli_qp_dpsub(*primals, tangents)
-        dq_dpsup, dp_dpsup = ddbernoulli_qp_dpsup(*primals, tangents)
+        dq_darea, dp_darea = bernmin.ddbernoulli_qp_darea(*primals, tangents)
+        dq_dpsub, dp_dpsub = bernmin.ddbernoulli_qp_dpsub(*primals, tangents)
+        dq_dpsup, dp_dpsup = bernmin.ddbernoulli_qp_dpsup(*primals, tangents)
+
+        mats = [
+            [-dq_darea, -dq_dpsub, -dq_dpsup],
+            [-dp_darea, -dp_dpsub, -dp_dpsup]]
+        return bla.BlockMatrix(mats, labels=(self.state.labels+self.control.labels))
+
+    def assem_dres_dprops(self):
+        # Depack variables from BlockVector for input to Bernoulli functions
+        # area = self.control['area']
+        # rho = self.props['rho_air'][0]
+        # psub = self.control['psub']
+        # psup = self.control['psup']
+        # zeta_min = self.props['zeta_min'][0]
+        # zeta_sep = self.props['zeta_sep'][0]
+        # primals = (area, self.s, psub, psup, rho, zeta_min, zeta_sep)
+
+        mats = [
+            [np.zeros((state_subvec.size, prop_subvec.size))
+                for prop_subvec in self.props]
+            for state_subvec in self.state]
+        return bla.BlockMatrix(mats, labels=(self.state.labels+self.props.labels))
+
+
+class BaseBernoulliFixedSeparation(BaseFluid1DDynamicalSystem):
+
+    def __init__(self, s):
+        super().__init__(s)
+
+        N = self.s.size
+        self.q = np.zeros(1)
+        self.p = np.zeros(N)
+        self.state = bla.BlockVector([self.q, self.p], labels=[['q', 'p']])
+
+        self.qt = np.zeros(1)
+        self.pt = np.zeros(N)
+        self.statet = bla.BlockVector([self.qt, self.pt], labels=[['q', 'p']])
+
+        self.control = bla.BlockVector([np.ones(N), np.zeros(1), np.zeros(1)], labels=[['area', 'psub', 'psup']])
+
+        self.dstate = self.state.copy()
+
+        self.dstatet = self.statet.copy()
+
+        self.dcontrol = self.control.copy()
+
+        properties_vec = ['rho_air', 'idx_sep']
+        self.props = bla.BlockVector(
+            [np.ones(1) for i in range(len(properties_vec))], labels=[properties_vec])
+
+class BernoulliFixedSeparation(BaseBernoulliFixedSeparation):
+    def assem_res(self):
+        # Depack variables from BlockVector for input to Bernoulli functions
+        area = self.control['area']
+        rho = self.props['rho_air'][0]
+        psub = self.control['psub'][0]
+        psup = self.control['psup'][0]
+        idx_sep = int(self.props['idx_sep'][0])
+
+        primals = (area, self.s, psub, psup, rho, idx_sep)
+        qp_explicit = bernfix.bernoulli_qp(*primals)
+        resq = self.q - qp_explicit[0]
+        resp = self.p - qp_explicit[1]
+        return bla.BlockVector([resq, resp], labels=self.state.labels)
+
+    def assem_dres_dstate(self):
+        dresq_dq = np.diag(np.ones(self.q.size))
+        dresq_dp = np.zeros((self.q.size, self.p.size))
+
+        dresp_dq = np.zeros((self.p.size, self.q.size))
+        dresp_dp = np.diag(np.ones(self.p.size))
+        mats = [
+            [dresq_dq, dresq_dp],
+            [dresp_dq, dresp_dp]]
+        return bla.BlockMatrix(mats, labels=self.state.labels+self.state.labels)
+
+    def assem_dres_dstatet(self):
+        dresq_dq = np.diag(np.zeros(self.q.size))
+        dresq_dp = np.zeros((self.q.size, self.p.size))
+
+        dresp_dp = np.diag(np.zeros(self.p.size))
+        dresp_dq = np.zeros((self.p.size, self.q.size))
+        mats = [
+            [dresq_dq, dresq_dp],
+            [dresp_dq, dresp_dp]]
+        return bla.BlockMatrix(mats, labels=self.state.labels+self.state.labels)
+
+    def assem_dres_dcontrol(self):
+        # Depack variables from BlockVector for input to Bernoulli functions
+        area = self.control['area']
+        rho = self.props['rho_air'][0]
+        psub = self.control['psub'][0]
+        psup = self.control['psup'][0]
+        idx_sep = int(self.props['idx_sep'][0])
+
+        primals = (area, self.s, psub, psup, rho, idx_sep)
+
+        dq_darea, dp_darea = bernfix.dbernoulli_qp_darea(*primals)
+        dq_dpsub, dp_dpsub = bernfix.dbernoulli_qp_dpsub(*primals)
+        dq_dpsup, dp_dpsup = bernfix.dbernoulli_qp_dpsup(*primals)
+
+        mats = [
+            [-dq_darea, -dq_dpsub, -dq_dpsup],
+            [-dp_darea, -dp_dpsub, -dp_dpsup]]
+        return bla.BlockMatrix(mats, labels=self.state.labels+self.control.labels)
+
+    def assem_dres_dprops(self):
+        # Depack variables from BlockVector for input to Bernoulli functions
+        mats = [
+            [np.zeros((state_subvec.size, prop_subvec.size))
+                for prop_subvec in self.props]
+            for state_subvec in self.state]
+        return bla.BlockMatrix(mats, labels=(self.state.labels[0], self.props.labels[0]))
+
+class LinearizedBernoulliFixedSeparation(BaseBernoulliFixedSeparation):
+    def assem_res(self):
+        # Depack variables from BlockVector for input to Bernoulli functions
+        area = self.control['area']
+        rho = self.props['rho_air'][0]
+        psub = self.control['psub'][0]
+        psup = self.control['psup'][0]
+        idx_sep = int(self.props['idx_sep'][0])
+        primals = (area, self.s, psub, psup, rho, idx_sep)
+
+        darea = self.dcontrol['area']
+        dpsub = self.dcontrol['psub'][0]
+        dpsup = self.dcontrol['psup'][0]
+        tangents = (darea, np.zeros(self.s.shape), dpsub, dpsup, 0.0, 0)
+
+        # The linearized residual changes due to linearizations
+        resq_bernoulli, resp_bernoulli = bernfix.dbernoulli_qp(*primals, tangents)
+
+        resq = self.dstate['q'] - resq_bernoulli
+        resp = self.dstate['p'] - resp_bernoulli
+
+        return bla.BlockVector([resq, resp], labels=self.state.labels)
+
+    def assem_dres_dstate(self):
+        dresq_dq = np.diag(np.zeros(self.q.size))
+        dresq_dp = np.zeros((self.q.size, self.p.size))
+
+        dresp_dp = np.diag(np.zeros(self.p.size))
+        dresp_dq = np.zeros((self.p.size, self.q.size))
+        mats = [
+            [dresq_dq, dresq_dp],
+            [dresp_dq, dresp_dp]]
+        return bla.BlockMatrix(mats, labels=self.state.labels+self.state.labels)
+
+    def assem_dres_dstatet(self):
+        dresq_dq = np.diag(np.zeros(self.q.size))
+        dresq_dp = np.zeros((self.q.size, self.p.size))
+
+        dresp_dp = np.diag(np.zeros(self.p.size))
+        dresp_dq = np.zeros((self.p.size, self.q.size))
+        mats = [
+            [dresq_dq, dresq_dp],
+            [dresp_dq, dresp_dp]]
+        return bla.BlockMatrix(mats, labels=self.state.labels+self.state.labels)
+
+    def assem_dres_dcontrol(self):
+        # Depack variables from BlockVector for input to Bernoulli functions
+        area = self.control['area']
+        rho = self.props['rho_air'][0]
+        psub = self.control['psub'][0]
+        psup = self.control['psup'][0]
+        idx_sep = int(self.props['idx_sep'][0])
+        primals = (area, self.s, psub, psup, rho, idx_sep)
+
+        darea = self.dcontrol['area']
+        dpsub = self.dcontrol['psub']
+        dpsup = self.dcontrol['psup']
+        tangents = (darea, np.zeros(self.s.shape), dpsub, dpsup, 0.0, 0)
+
+        dq_darea, dp_darea = bernfix.ddbernoulli_qp_darea(*primals, tangents)
+        dq_dpsub, dp_dpsub = bernfix.ddbernoulli_qp_dpsub(*primals, tangents)
+        dq_dpsup, dp_dpsup = bernfix.ddbernoulli_qp_dpsup(*primals, tangents)
 
         mats = [
             [-dq_darea, -dq_dpsub, -dq_dpsup],
---
+++ femvf/models/equations/fluid/bernoulli_sep_at_fixed.py
@@ -0,0 +1,76 @@
+"""
+This module contains the equations defining a quasi-steady Bernoulli fluid with
+separation at a fixed location.
+"""
+import numpy as np
+from jax import numpy as jnp
+import jax
+
+## To disable jax:
+# jnp = np
+
+# pylint: disable=abstract-method
+# @jax.jit
+def bernoulli_q(asep, psub, psup, rho):
+    """
+    Return the flow rate based on Bernoulli
+    """
+    flow_sign = jnp.sign(psub-psup)
+    q = flow_sign * (2*jnp.abs(psub-psup)/rho)**0.5 * asep
+    return q
+
+# @jax.jit
+def bernoulli_p(q, area, psub, psup, rho):
+    """
+    Return the pressure based on Bernoulli
+    """
+    p = psub - 1/2*rho*(q/area)**2
+    return p
+
+def coeff_sep(idx_sep: int, n: int):
+    """
+    Return a weighting representing cut-off at the separation point
+
+    Parameters
+    ----------
+    idx_sep :
+        Node where separation occurs
+    n :
+        Number of nodes in the mesh
+    """
+    f = np.ones((n,))
+    f[idx_sep+1:] = 0
+    return f
+
+@jax.jit
+def bernoulli_qp(area, s, psub, psup, rho, idx_sep):
+    """
+    Return Bernoulli flow and pressure
+    """
+    asep = area[idx_sep]
+    # ssep = s[idx_sep]
+    q = bernoulli_q(asep, psub, psup, rho)
+    p = bernoulli_p(q, area, psub, psup, rho)
+
+    # Separation coefficient ensure pressures tends to zero after separation
+    f_sep = coeff_sep(idx_sep, area.size)
+    p = f_sep * p
+    return q, p
+
+# Use this weird combination of primals/tangent parameters because later we
+# want to compute the jacobian wrt area
+def dbernoulli_qp(area, s, psub, psup, rho, idx_sep, tangents):
+    """
+    Return linearization of Bernoulli flow and pressure
+    """
+    primals = (area, s, psub, psup, rho, idx_sep)
+    return jax.jvp(bernoulli_qp, primals, tangents)[1]
+
+dbernoulli_qp_darea = jax.jacfwd(bernoulli_qp, argnums=0)
+ddbernoulli_qp_darea = jax.jacfwd(dbernoulli_qp, argnums=0)
+
+dbernoulli_qp_dpsub = jax.jacfwd(bernoulli_qp, argnums=2)
+ddbernoulli_qp_dpsub = jax.jacfwd(dbernoulli_qp, argnums=2)
+
+dbernoulli_qp_dpsup = jax.jacfwd(bernoulli_qp, argnums=3)
+ddbernoulli_qp_dpsup = jax.jacfwd(dbernoulli_qp, argnums=3)
