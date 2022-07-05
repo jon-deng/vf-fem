319739e11142a858daf2aaad3abad567d38cdf44
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-07-04 20:06:40 -0400

    Add initial layer moduli parameterization

--- femvf/parameters/parameterization.py
+++ femvf/parameters/parameterization.py
@@ -2,21 +2,15 @@
 Contains definitions of parametrizations. These objects should provide a mapping from their specific parameters to standardized parameters of the forward model, as well as the derivative of the map.
 """
 
-import math
-
-from collections import OrderedDict
+import numpy as np
+from jax import numpy as jnp
 import jax
-
-from blockarray import blockvec as bv
-from . import properties as props
-from .. import constants, linalg
-from ..solverconst import DEFAULT_NEWTON_SOLVER_PRM
-
 import dolfin as dfn
 import ufl
+from femvf import meshutils
 from petsc4py import PETSc
 
-import numpy as np
+from blockarray import blockvec as bv
 
 class Parameterization:
     """
@@ -44,10 +38,14 @@ class Parameterization:
         The BlockVector representation of the parameterization
     """
 
-    def __init__(self, model, *args, **kwargs):
+    def __init__(self, model, out_default, *args, **kwargs):
         self.model = model
         self._out_labels = model.props.labels
-        # self._out_bshape = model.props.bshape
+        self._out_default = out_default
+        self.in_vec, self.map = self.make_map()
+
+    def make_map(self):
+        raise NotImplementedError()
 
     def apply(self, in_vec: bv.BlockVector) -> bv.BlockVector:
         """
@@ -85,4 +83,37 @@ class Parameterization:
         out_dict = jax.jvp(self.map, in_dict, din_dict)
         out_subvecs = [out_dict[label] for label in self._out_labels]
         return bv.BlockVector(out_subvecs, labels=self._out_labels)
+
+class LayerModuli(Parameterization):
+
+    def make_map(self):
+        ## Get the mapping from labelled cell regions to DOFs
+        # mesh = self.model.solid.forms['mesh.mesh']
+        # cell_func = self.model.solid.forms['mesh.cell_function']
+        # cell_label_to_id = self.model.solid.forms['mesh.cell_label_to_id']
+        E = self.model.solid.forms['coeff.prop.emod']
+        cell_label_to_dofs = meshutils.process_celllabel_to_dofs_from_forms(
+            self.model.solid.forms,
+            E.function_space()
+        )
+
+        out_labels = self._out_labels
+        out_subvecs = [self._out_default[label] for label in out_labels]
+        out_dict = {label: subvec for label, subvec in zip(out_subvecs, out_labels)}
+        def map(x):
+
+            new_emod = jnp.array(out_dict['emod'], copy=True)
+            for label, value in x.items():
+                dofs = cell_label_to_dofs[label]
+                new_emod.at[dofs] = value
+
+            out_dict['emod'] = new_emod
+            return out_dict
+
+        ## Define the input vector
+        labels = list(cell_label_to_dofs.keys())
+        subvecs = [np.zeros(1) for _ in labels]
+        in_vec = bv.BlockVector(subvecs, labels=labels)
+
+        return in_vec, map
 
