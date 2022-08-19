486084f9206ae955cbc1a7be498e72db1fcfa577
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-08-19 11:51:27 -0400

    bug: Fix bad call to `Model.res`

--- femvf/stepfunctional/base.py
+++ femvf/stepfunctional/base.py
@@ -8,13 +8,13 @@ def make_residuals(model):
     MODEL_ATTR_NAMES = ('solid', 'fluid', 'acoustic')
     SUB_MODELS = tuple([getattr(model, name) for name in MODEL_ATTR_NAMES
                         if hasattr(model, name)])
-    
+
     def residuals(ini_state, fin_state, control, dt, props):
         model.set_ini_state(ini_state)
         model.set_fin_state(fin_state)
         model.set_control(control)
         model.dt = dt
 
-        return np.array([model.res().norm() for model in SUB_MODELS])
+        return np.array([model.assem_res().norm() for model in SUB_MODELS])
 
     return residuals
