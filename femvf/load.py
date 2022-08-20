ec81e928def50e40c7be876a452e3117b5ccf6a7
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-08-20 18:39:57 -0400

    doc: Add typing and docstrings to load.py

--- femvf/load.py
+++ femvf/load.py
@@ -1,33 +1,48 @@
 """
 Functionality for creating model objects from meshes, etc.
 """
+from lib2to3.pgen2.token import OP
 from os import path
+from typing import Union, Optional, List, Type, Any, Tuple
 import numpy as np
 import dolfin as dfn
 
 from . import meshutils
-from .models.transient import solid as tsmd, fluid as tfmd
-from .models.transient.coupled import (FSAIModel, ExplicitFSIModel, ImplicitFSIModel)
+from .models.transient import solid as tsmd, fluid as tfmd, acoustic as tamd, coupled as tcmd
+from .models.dynamical import solid as dsmd, fluid as dfmd, coupled as dcmd
 
-from .models.dynamical import solid as dsmd, fluid as dfmd, coupled as dynfsimodel
+SolidModel = Union[tsmd.Solid, dsmd.BaseSolidDynamicalSystem]
+FluidModel = Union[tfmd.QuasiSteady1DFluid, dfmd.BaseFluid1DDynamicalSystem]
+SolidClass = Union[Type[tsmd.Solid], Type[dsmd.BaseSolidDynamicalSystem]]
+FluidClass = Union[Type[tfmd.QuasiSteady1DFluid], Type[dfmd.BaseFluid1DDynamicalSystem]]
 
-# from .dynamicalmodels.coupled import (FSAIModel, ExplicitFSIModel, ImplicitFSIModel)
+Labels = List[str]
 
 def load_solid_model(
-        solid_mesh,
-        SolidType,
-        pressure_facet_labels=('pressure',),
-        fixed_facet_labels=('fixed',)
-    ):
+        solid_mesh: str,
+        SolidType: SolidClass,
+        pressure_facet_labels: Optional[Labels]=('pressure',),
+        fixed_facet_labels: Optional[Labels]=('fixed',)
+    ) -> SolidModel:
     """
-    Loads a solid model
+    Load a solid model of the specified type
+
+    Parameters
+    ----------
+    solid_mesh:
+        A string indicating the path to a mesh file. This can be either in
+         '.xml' or '.msh' format.
+    SolidType:
+        A class indicating the type of solid model to load.
+    pressure_facet_labels, fixed_facet_labels:
+        Lists of strings for labelled facets corresponding to the pressure
+        loading and fixed boundaries.
     """
-    # Process the mesh file(s)
     if isinstance(solid_mesh, str):
         ext = path.splitext(solid_mesh)[1]
-        # if no extension is supplied, assume it's a .msh file from gmsh
+        # If no extension is supplied, assume it's a .msh file from gmsh
         if ext == '':
-            ext = '.xml'
+            ext = '.msh'
 
         if ext.lower() == '.xml':
             # The solid mesh is an xml file
@@ -42,26 +57,38 @@ def load_solid_model(
 
     return SolidType(mesh, mesh_funcs, mesh_entities_label_to_value, pressure_facet_labels, fixed_facet_labels)
 
+# TODO: Can combine transient/dynamical model loading functions into single one
 def load_transient_fsi_model(
-        solid_mesh,
-        fluid_mesh,
-        SolidType=tsmd.KelvinVoigt,
-        FluidType=tfmd.BernoulliAreaRatioSep,
-        fsi_facet_labels=('pressure',),
-        fixed_facet_labels=('fixed',),
-        separation_vertex_label='separation',
-        coupling='explicit'
-    ):
+        solid_mesh: str,
+        fluid_mesh: Any,
+        SolidType: SolidClass=tsmd.KelvinVoigt,
+        FluidType: FluidClass=tfmd.BernoulliAreaRatioSep,
+        fsi_facet_labels: Optional[Labels]=('pressure',),
+        fixed_facet_labels: Optional[Labels]=('fixed',),
+        separation_vertex_label: str='separation',
+        coupling: str='explicit'
+    ) -> tcmd.FSIModel:
     """
-    Factory function that loads a model based on input solid/fluid meshes.
-
-    The function will delegate to specific loading routines depending on how the mesh
-    is input etc.
+    Load a transient coupled (fsi) model
 
     Parameters
     ----------
-    solid_mesh : dolfin.Mesh
+    solid_mesh : str
+        Path to the solid mesh
     fluid_mesh : None or (future proofing for other fluid types)
+        Currently this isn't used
+    SolidType, FluidType:
+        Classes of the solid and fluid models to load
+    fsi_facet_labels, fixed_facet_labels:
+        String identifiers for facets corresponding to traction/dirichlet
+        conditions
+    separation_vertex_label:
+        A string corresponding to a labelled vertex where separation should
+        occur. This is only relevant for quasi-static fluid models with a fixed
+        separation point
+    coupling: str
+        One of 'explicit' or 'implicit' indicating the coupling strategy between
+        fluid/solid domains
     """
     solid, fluid, fsi_verts = process_fsi(
         solid_mesh, fluid_mesh,
@@ -69,37 +96,53 @@ def load_transient_fsi_model(
         fsi_facet_labels=fsi_facet_labels,
         fixed_facet_labels=fixed_facet_labels,
         separation_vertex_label=separation_vertex_label
-        )
+    )
 
+    # TODO: This FSI dof selection won't for higher order elements
     dofs_fsi_solid = dfn.vertex_to_dof_map(solid.forms['fspace.scalar'])[fsi_verts]
     dofs_fsi_fluid = np.arange(dofs_fsi_solid.size)
 
     if coupling == 'explicit':
-        model = ExplicitFSIModel(solid, fluid, dofs_fsi_solid, dofs_fsi_fluid)
+        model = tcmd.ExplicitFSIModel(solid, fluid, dofs_fsi_solid, dofs_fsi_fluid)
     elif coupling == 'implicit':
-        model = ImplicitFSIModel(solid, fluid, dofs_fsi_solid, dofs_fsi_fluid)
+        model = tcmd.ImplicitFSIModel(solid, fluid, dofs_fsi_solid, dofs_fsi_fluid)
+    else:
+        raise ValueError(
+            f"'coupling' must be one of [explicit, implicit], not {coupling}"
+        )
 
     return model
 
 def load_dynamical_fsi_model(
-        solid_mesh,
-        fluid_mesh,
-        SolidType=dsmd.KelvinVoigt,
-        FluidType=dfmd.BernoulliAreaRatioSep,
-        fsi_facet_labels=('pressure',),
-        fixed_facet_labels=('fixed',),
-        separation_vertex_label='separation'
-    ):
+        solid_mesh: str,
+        fluid_mesh: Any,
+        SolidType: SolidClass=dsmd.KelvinVoigt,
+        FluidType: FluidClass=dfmd.BernoulliAreaRatioSep,
+        fsi_facet_labels: Optional[Labels]=('pressure',),
+        fixed_facet_labels: Optional[Labels]=('fixed',),
+        separation_vertex_label: str='separation'
+    ) -> dcmd.DynamicalSystem:
     """
-    Factory function that loads a model based on input solid/fluid meshes.
-
-    The function will delegate to specific loading routines depending on how the mesh
-    is input etc.
+    Load a transient coupled (fsi) model
 
     Parameters
     ----------
-    solid_mesh : dolfin.Mesh
+    solid_mesh : str
+        Path to the solid mesh
     fluid_mesh : None or (future proofing for other fluid types)
+        Currently this isn't used
+    SolidType, FluidType:
+        Classes of the solid and fluid models to load
+    fsi_facet_labels, fixed_facet_labels:
+        String identifiers for facets corresponding to traction/dirichlet
+        conditions
+    separation_vertex_label:
+        A string corresponding to a labelled vertex where separation should
+        occur. This is only relevant for quasi-static fluid models with a fixed
+        separation point
+    coupling: str
+        One of 'explicit' or 'implicit' indicating the coupling strategy between
+        fluid/solid domains
     """
     solid, fluid, fsi_verts = process_fsi(
         solid_mesh, fluid_mesh,
@@ -107,25 +150,44 @@ def load_dynamical_fsi_model(
         fsi_facet_labels=fsi_facet_labels,
         fixed_facet_labels=fixed_facet_labels,
         separation_vertex_label=separation_vertex_label
-        )
+    )
 
     dofs_fsi_solid = dfn.vertex_to_dof_map(solid.forms['fspace.scalar'])[fsi_verts]
     dofs_fsi_fluid = np.arange(dofs_fsi_solid.size)
 
-    return dynfsimodel.FSIDynamicalSystem(solid, fluid, dofs_fsi_solid, dofs_fsi_fluid)
+    return dcmd.FSIDynamicalSystem(solid, fluid, dofs_fsi_solid, dofs_fsi_fluid)
 
 def load_transient_fsai_model(
-        solid_mesh,
-        fluid_mesh,
-        acoustic,
-        SolidType=tsmd.KelvinVoigt,
-        FluidType=tfmd.BernoulliAreaRatioSep,
-        fsi_facet_labels=('pressure',),
-        fixed_facet_labels=('fixed',),
-        coupling='explicit'
+        solid_mesh: str,
+        fluid_mesh: Any,
+        acoustic: tamd.Acoustic1D,
+        SolidType: SolidClass=tsmd.KelvinVoigt,
+        FluidType: FluidClass=tfmd.BernoulliAreaRatioSep,
+        fsi_facet_labels: Optional[Labels]=('pressure',),
+        fixed_facet_labels: Optional[Labels]=('fixed',),
+        coupling: str='explicit'
     ):
+    # TODO: I haven't updated the acoustic model in a while so it's likely this
+    # doesn't work
     """
-    Loads a transient FSAI model
+    Load a transient coupled (fsai) model
+
+    Parameters
+    ----------
+    solid_mesh : str
+        Path to the solid mesh
+    fluid_mesh : None or (future proofing for other fluid types)
+        Currently this isn't used
+    acoustic :
+        The acoustic model
+    SolidType, FluidType:
+        Classes of the solid and fluid models to load
+    fsi_facet_labels, fixed_facet_labels:
+        String identifiers for facets corresponding to traction/dirichlet
+        conditions
+    coupling: str
+        One of 'explicit' or 'implicit' indicating the coupling strategy between
+        fluid/solid domains
     """
     solid, fluid, fsi_verts = process_fsi(
         solid_mesh, fluid_mesh,
@@ -137,19 +199,37 @@ def load_transient_fsai_model(
     dofs_fsi_solid = dfn.vertex_to_dof_map(solid.forms['fspace.scalar'])[fsi_verts]
     dofs_fsi_fluid = np.arange(dofs_fsi_solid.size)
 
-    return FSAIModel(solid, fluid, acoustic, dofs_fsi_solid, dofs_fsi_fluid)
+    return tcmd.FSAIModel(solid, fluid, acoustic, dofs_fsi_solid, dofs_fsi_fluid)
 
+# TODO: Refactor this function; currently does too many things
+# the function should take a loaded solid model and derive a fluid mesh from it
 def process_fsi(
-        solid_mesh,
-        fluid_mesh,
-        SolidType=tsmd.KelvinVoigt,
-        FluidType=tfmd.BernoulliAreaRatioSep,
-        fsi_facet_labels=('pressure',),
-        fixed_facet_labels=('fixed',),
-        separation_vertex_label='separation',
-    ):
+        solid_mesh: str,
+        fluid_mesh: Any,
+        SolidType: SolidClass=tsmd.KelvinVoigt,
+        FluidType: FluidClass=tfmd.BernoulliAreaRatioSep,
+        fsi_facet_labels: Optional[Labels]=('pressure',),
+        fixed_facet_labels: Optional[Labels]=('fixed',),
+        separation_vertex_label: str='separation',
+    ) -> Tuple[SolidModel, FluidModel, np.ndarray]:
     """
     Processes appropriate mappings between fluid/solid domains for FSI
+
+    Parameters
+    ----------
+    solid_mesh : str
+        Path to the solid mesh
+    fluid_mesh : None or (future proofing for other fluid types)
+        Currently this isn't used
+    SolidType, FluidType:
+        Classes of the solid and fluid models to load
+    fsi_facet_labels, fixed_facet_labels:
+        String identifiers for facets corresponding to traction/dirichlet
+        conditions
+    separation_vertex_label:
+        A string corresponding to a labelled vertex where separation should
+        occur. This is only relevant for quasi-static fluid models with a fixed
+        separation point
     """
     ## Load the solid
     solid = load_solid_model(solid_mesh, SolidType, fsi_facet_labels, fixed_facet_labels)
@@ -189,29 +269,39 @@ def process_fsi(
             vertex_label_to_id = solid.forms['mesh.vertex_label_to_id']
             vertex_mf = solid.forms['mesh.vertex_function']
             if vertex_mf is None:
-                raise ValueError("Loading a fixed separation point fluid model but solid mesh doesn't specify a separation point")
+                raise ValueError(
+                    f"Couldn't find separation point label {separation_vertex_label}"
+                )
 
             if separation_vertex_label in vertex_label_to_id:
                 sep_mf_value = vertex_label_to_id[separation_vertex_label]
                 # assert isinstance(sep_mf_value, int)
             else:
-                raise ValueError("Loading a fixed separation point fluid model but solid mesh doesn't specify a separation point")
+                raise ValueError(
+                    f"Couldn't find separation point label {separation_vertex_label}"
+                )
 
             sep_vert = vertex_mf.where_equal(sep_mf_value)
             if len(sep_vert) == 1:
                 sep_vert = sep_vert[0]
             else:
-                raise ValueError(f"Single separation point expected but {len(sep_vert):d} were supplied")
+                raise ValueError(
+                    "A single separation point was expected but"
+                    f" {len(sep_vert):d} were supplied"
+                )
 
             fsi_verts_fluid_ord = np.arange(fsi_verts.size)
             idx_sep = fsi_verts_fluid_ord[fsi_verts == sep_vert]
             if len(idx_sep) == 1:
                 idx_sep = idx_sep[0]
             else:
-                raise ValueError(f"Expected to find single separation point on FSI surface but found {len(idx_sep):d} instead")
+                raise ValueError(
+                    "Expected to find single separation point on FSI surface"
+                    f" but found {len(idx_sep):d} instead"
+                )
             fluid = FluidType(s, idx_sep=idx_sep)
         else:
             fluid = FluidType(s)
     else:
-        raise ValueError(f"This function does not yet support input fluid meshes.")
+        raise ValueError("This function does not yet support input fluid meshes.")
     return solid, fluid, fsi_verts
