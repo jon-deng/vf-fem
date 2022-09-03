bb294289ed95e9e3e754f90dc517ae88993fdbb4
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-09-03 13:40:59 -0400

    feat: Add 'benchmark_postprocess.py' script

---
+++ benchmarks/benchmark_postprocess.py
@@ -0,0 +1,42 @@
+"""
+Benchmark `femvf.postprocess`
+"""
+
+from os import path
+
+import numpy as np
+
+from blockarray import (blockvec as bv)
+
+from femvf import (statefile as sf, forward)
+from femvf.postprocess import (solid as ppsld, base as ppbase)
+
+from setup import setup_model, setup_transient_args
+from benchmarkutils import setup_argument_parser, benchmark
+
+if __name__ == '__main__':
+    parser = setup_argument_parser()
+    args = parser.parse_args()
+
+    model = setup_model('../meshes/M5-3layers.msh')
+    state0, control, props = setup_transient_args(model)
+
+    _times = 1e-4*np.arange(100)
+    times = bv.BlockVector((_times,), (1,))
+
+    fpath = './out/.benchmark_postprocess.h5'
+    if not path.isfile(fpath):
+        with sf.StateFile(model, fpath, mode='w') as f:
+            forward.integrate(model, f, state0, [control], props, times, write=True)
+    else:
+        print("Using existing `StateFile` to post-process")
+
+    postproc_field = ppsld.StressVonMisesField(model)
+    postproc_ts_field = ppbase.TimeSeries(postproc_field)
+
+    with sf.StateFile(model, fpath, mode='r') as f:
+        statement = 'postproc_ts_field(f)'
+        benchmark(
+            statement, './benchmark_postprocess.profile',
+            profile=args.profile, globals=globals()
+        )
