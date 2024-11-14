b9b6fccdf4b763a77271d1b0d0f74d440d32a625
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-11-14 10:37:47 -0500

    tests: Add more `FenicsForms` tests

--- tests/models/equations/test_solid.py
+++ tests/models/equations/test_solid.py
@@ -8,7 +8,7 @@ import dolfin as dfn
 
 from femvf.models.equations import solid as sld
 
-class TestFenicsForm:
+class UFLFormFixtures:
 
     @pytest.fixture()
     def mesh(self):
@@ -16,7 +16,15 @@ class TestFenicsForm:
         return mesh
 
     @pytest.fixture()
-    def form_tuple(self, mesh):
+    def measure_dx(self, mesh):
+        return dfn.Measure("dx", mesh)
+
+    @pytest.fixture()
+    def measure_ds(self, mesh):
+        return dfn.Measure("ds", mesh)
+
+    @pytest.fixture()
+    def ufl_form_tuple(self, mesh, measure_dx):
         FSPACE_V = dfn.FunctionSpace(mesh, "CG", 1)
 
         u = dfn.TrialFunction(FSPACE_V)
@@ -25,13 +33,54 @@ class TestFenicsForm:
         source = dfn.Function(FSPACE_V)
         source.vector()[:] = 1.0
 
-        dx = dfn.Measure("dx", mesh)
-        form = (dfn.inner(dfn.grad(u), dfn.grad(v)) - source) * dx
+        form = (dfn.inner(dfn.grad(u), dfn.grad(v)) - source) * measure_dx
         coefficients = {"u": u, "source": source}
         return form, coefficients
 
-    def test_init(self, form_tuple):
-        form, coefficients = form_tuple
+    @pytest.fixture()
+    def ufl_form(self, ufl_form_tuple):
+        ufl_form, _ = ufl_form_tuple
+        return ufl_form
 
-        assert sld.FenicsForm(form, coefficients)
+    @pytest.fixture()
+    def ufl_coefficients(self, ufl_form_tuple):
+        _, coefficients = ufl_form_tuple
+        return coefficients
+
+
+class TestFenicsForm(UFLFormFixtures):
+
+    def test_init(self, ufl_form, ufl_coefficients):
+        assert sld.FenicsForm(ufl_form, ufl_coefficients)
+
+
+class TestPredefinedVolumeForms(UFLFormFixtures):
+
+    @pytest.fixture(
+        params = [
+            sld.IsotropicElasticForm,
+            sld.IsotropicIncompressibleElasticSwellingForm,
+            sld.IsotropicElasticSwellingForm,
+            sld.IsotropicElasticSwellingPowerLawForm
+        ]
+    )
+    def CellForm(self, request):
+        return request.param
+
+    def test_CellForm(self, CellForm, measure_dx, mesh):
+        assert CellForm({}, measure_dx, mesh)
+
+    @pytest.fixture(
+        params = [
+            sld.IsotropicMembraneForm,
+            sld.IsotropicIncompressibleMembraneForm,
+            sld.SurfacePressureForm,
+            sld.ManualSurfaceContactTractionForm
+        ]
+    )
+    def FacetForm(self, request):
+        return request.param
+
+    def test_FacetForm(self, FacetForm, measure_ds, mesh):
+        assert FacetForm({}, measure_ds, mesh)
 
