23eaeec564312c2d82fb20bdcd9dbd69453b35e2
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-11-14 12:53:03 -0500

    tests: Add fluid residual tests

--- tests/models/equations/test_fluid.py
+++ tests/models/equations/test_fluid.py
@@ -27,3 +27,60 @@ class TestJaxResidual:
     def test_JaxResidual(self, res_descr):
         res, res_args = res_descr
         assert fld.JaxResidual(res, res_args)
+
+
+class MeshFixture:
+
+    @pytest.fixture()
+    def mesh(self):
+        s = np.linspace(0, 1, 11)
+        return s
+
+
+class TestBernoulliFixedSep(MeshFixture):
+
+    @pytest.fixture()
+    def area(self, mesh):
+        # Simulate a triangular constriction for Bernoulli
+        area = np.abs(mesh-0.5)
+        min_area = 0.1
+        area[area < min_area] = min_area
+        return area
+
+    def test_BernoulliFixedSep(self, mesh):
+        assert fld.BernoulliFixedSep(mesh, idx_sep=mesh.shape[-1]//2)
+
+    @pytest.fixture()
+    def residual(self, mesh):
+        return fld.BernoulliFixedSep(mesh, idx_sep=mesh.shape[-1]//2)
+
+    def test_res(self, mesh, residual, area):
+        state, control, prop = residual.res_args
+
+        state['q'] = np.zeros(state['q'].shape)
+        state['p'] = np.zeros(state['p'].shape)
+
+        control['area'] = area
+        control['psub'] = 100*np.ones(mesh.shape[:-1])
+        control['psup'] = 0*np.ones(mesh.shape[:-1])
+        print(control)
+        res = residual.res(state, control, prop)
+        print(res)
+
+        # NOTE: the 'q' and 'p' residuals should reflect the bernoulli flow/pressure
+        # for the given area function
+
+
+class TestBernoulliSmoothMinSep(MeshFixture):
+
+    def test_BernoulliSmoothMinSep(self, mesh):
+        assert fld.BernoulliSmoothMinSep(mesh)
+
+    # TODO: Add similar tests to `TestBernoulliFixedSep` here!
+
+
+class TestBernoulliAreaRatioSep(MeshFixture):
+
+    def test_BernoulliAreaRatioSep(self, mesh):
+        assert fld.BernoulliAreaRatioSep(mesh)
+
