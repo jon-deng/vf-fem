9a8ed4ff6d02c4331ce8db88cb5d76c3fec1e4ba
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-11-14 21:13:45 -0500

    tests: Move base residual tests to 'test_base.py'

--- tests/residuals/test_fluid.py
+++ tests/residuals/test_fluid.py
@@ -4,29 +4,9 @@ Test fluid model form definitions
 
 import pytest
 
-from numpy.typing import NDArray
-
 import numpy as np
-from jax import numpy as jnp
 
 from femvf.residuals import fluid as fld
-
-
-class TestJaxResidual:
-
-    @pytest.fixture()
-    def res_descr(self):
-
-        def res(state: dict[str, NDArray], coefficients: dict[str, NDArray]):
-            return state['f'] - coefficients['source']
-
-        res_args = ({'f': np.zeros((10,))}, {'source': np.ones((10,))})
-
-        return res, res_args
-
-    def test_JaxResidual(self, res_descr):
-        res, res_args = res_descr
-        assert fld.JaxResidual(res, res_args)
 
 
 class MeshFixture:
