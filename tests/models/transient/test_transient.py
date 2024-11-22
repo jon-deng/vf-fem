8a3dd791bd32e576e381c47d81f753417b4632d7
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-11-22 12:47:18 -0500

    tests: Add tests for updated solid model init

--- tests/models/transient/test_transient.py
+++ tests/models/transient/test_transient.py
@@ -7,6 +7,7 @@ import pytest
 import numpy as np
 import dolfin as dfn
 
+from femvf.residuals import solid as slr
 from femvf.models.transient import solid as sld, fluid as fld, coupled as cpd
 from femvf.load import derive_1dfluid_from_2dsolid, derive_1dfluid_from_3dsolid
 
@@ -16,23 +17,24 @@ from tests.fixture_mesh import FenicsMeshFixtures
 class TestSolid(FenicsMeshFixtures):
 
     @pytest.fixture(
-        params=[sld.Rayleigh, sld.KelvinVoigt, sld.SwellingKelvinVoigt]
+        params=[slr.Rayleigh, slr.KelvinVoigt, slr.SwellingKelvinVoigt]
     )
     def SolidModel(self, request):
         return request.param
 
     def test_init(
             self,
-            SolidModel: sld.PredefinedModel,
+            SolidModel: slr.PredefinedSolidResidual,
             mesh,
-            vertex_function_tuple,
-            facet_function_tuple,
-            cell_function_tuple
+            mesh_functions,
+            mesh_subdomains
         ):
-        mf_tuples = [vertex_function_tuple, facet_function_tuple, cell_function_tuple]
-        mfs = [mf_tuple[0] for mf_tuple in mf_tuples]
-        mfs_values = [mf_tuple[1] for mf_tuple in mf_tuples]
-        assert SolidModel(mesh, mfs, mfs_values, fixed_facet_labels=['fixed'], fsi_facet_labels=['traction'])
+        dim = mesh.topology().dim()
+        dirichlet_bcs = {
+            'coeff.state.u1': [(dfn.Constant(dim*[0]), 'facet', 'fixed')]
+        }
+        residual = SolidModel(mesh, mesh_functions, mesh_subdomains, dirichlet_bcs)
+        assert sld.Model(residual)
 
     # TODO: Think of ways you can test a model is working properly?
 
@@ -51,7 +53,7 @@ class TestFluid:
 
     def test_init(
             self,
-            FluidModel: sld.PredefinedModel,
+            FluidModel,
             mesh
         ):
         assert FluidModel(mesh)
@@ -62,7 +64,7 @@ class TestFluid:
 class TestCoupled(FenicsMeshFixtures):
 
     @pytest.fixture(
-        params=[sld.Rayleigh, sld.KelvinVoigt, sld.SwellingKelvinVoigt]
+        params=[slr.Rayleigh, slr.KelvinVoigt, slr.SwellingKelvinVoigt]
     )
     def SolidModel(self, request):
         return request.param
