df8ec8998037415ee7743e00e570df6b36b284ea
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-11-22 12:12:29 -0500

    tests: Add tests for solid residual class init

--- tests/residuals/test_solid.py
+++ tests/residuals/test_solid.py
@@ -2,4 +2,39 @@
 Test solid model form definitions
 """
 
-# TODO: Add tests of residual definitions in `femvf.residuals.solid`
+import pytest
+
+import dolfin as dfn
+
+from femvf.residuals import solid
+
+from tests.fixture_mesh import FenicsMeshFixtures
+
+
+class TestResidual(FenicsMeshFixtures):
+    RESIDUAL_CLASSES = (
+        solid.Rayleigh,
+        solid.KelvinVoigt,
+        solid.SwellingKelvinVoigt,
+        solid.SwellingKelvinVoigtWEpithelium
+    )
+
+    @pytest.fixture(params=RESIDUAL_CLASSES)
+    def ResidualClass(self, request):
+        return request.param
+
+    def test_init(
+            self,
+            ResidualClass: solid.PredefinedFenicsResidual,
+            mesh: dfn.Mesh,
+            mesh_functions: list[dfn.MeshFunction],
+            mesh_subdomains
+        ):
+
+        dim = mesh.topology().dim()
+        dirichlet_bcs = {
+            'coeff.state.u1': [(dfn.Constant(dim*[0]), 'facet', 'fixed')],
+            # 'coeff.state.u0': [(dfn.Constant(dim*[0]), 'facet', 'fixed')]
+        }
+
+        ResidualClass(mesh, mesh_functions, mesh_subdomains, dirichlet_bcs)
